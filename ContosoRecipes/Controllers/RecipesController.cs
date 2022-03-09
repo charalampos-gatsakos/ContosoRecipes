1c9d4584eb77e51be26aba601a5f730ecb34c43f
cgatsakos <cgatsakos@CGATSAKOS-PC>
2022-03-09 17:34:44 +0200

    Add project files.

---
+++ ContosoRecipes/Controllers/RecipesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoRecipes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetDishes()
+        {
+            string[] dishes = { "Mararonia", "Kota Rizi", "Bithkoti" };
+            return dishes;
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteRecipes(string id)
+        {
+            bool bTh = false;
+            if (bTh)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+    }
+}
+ 
